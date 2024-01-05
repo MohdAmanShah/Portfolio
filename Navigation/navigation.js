748f619f7331815011ee273a948f70cbd36f0d4e
Mohd Aman Shah <ms_Noa999@outlook.com>
2024-01-05 15:13:36 +0530

    all devices done

--- Navigation/navigation.js
+++ Navigation/navigation.js
@@ -1,13 +1,25 @@
 export function makeHamburgerEvent() {
   var navbutton = document.getElementsByClassName("nav-toggle")[0];
-  navbutton.addEventListener("click", HandleEvent);
+  navbutton.addEventListener("click", HandleTogglerEvent);
 }
-
-function HandleEvent(e) {
+export function rendorActiveLink() {
+  var navLinks = document.getElementsByClassName("nav-link");
+  var linkArray = Array.from(navLinks);
+  linkArray.forEach((e, i, arr) => {
+    e.addEventListener("click", HandleLinkSelectEvent);
+  });
+}
+function HandleTogglerEvent(e) {
   document.getElementsByClassName("hamburger")[0].classList.toggle("pressed");
   document.getElementsByClassName("navigation")[0].classList.toggle("hide");
   document.getElementById("content").classList.toggle("blur");
   var cvbutton = document.getElementsByClassName("cvbutton")[0];
   cvbutton.classList.toggle("blur");
   cvbutton.classList.toggle("disabled");
+}
+
+function HandleLinkSelectEvent(e) {
+  var ActiveLink = document.getElementsByClassName("activeLink")[0];
+  ActiveLink.classList.toggle("activeLink");
+  e.target.ClassList.toggle("activeLink");
 }
