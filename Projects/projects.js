1406d1cd15cc6bbf59f686b5890adb3c6a6449b6
Mohd Aman Shah <reach.mohdamanshah@gmail.com>
2024-02-09 12:57:55 +0530

    f

--- Projects/projects.js
+++ Projects/projects.js
@@ -3,6 +3,7 @@ export async function RenderProjects() {
     let object = await fetch('./Projects/project.json')
     object = await object.text();
     object = JSON.parse(object);
+    console.log(object);
     let projects = document.getElementsByClassName('projects')[0]
     let html = "";
     for (var i = 0; i < object.projects.length; ++i) {
@@ -11,7 +12,7 @@ export async function RenderProjects() {
                 <img src="${object.projects[i].imageurl}" width="100px" />
             </div>
             <div class="details">
-                <h2 class="projectName">${object.projects[i].name}</h2>
+                <h2 class="projectName"><a href=${object.projects[i].Link}>${object.projects[i].name}</a></h2>
                 <p class="projectDesp">${object.projects[i].description}</p>
             </div>
         </div>`;
